0e9494715fe94026b6b6b1b535f2c52e4c6f8ae3
Xilong Pei <pei.xilong@kortide.com.cn>
2013-03-16 16:22:15 +0800

    wordpress: only super-admin can edit user's 'ElastosOrg setting', for it is for user management on elastos.org

--- redmine/apps/wordpress/htdocs/wp-admin/user-edit.php
+++ redmine/apps/wordpress/htdocs/wp-admin/user-edit.php
@@ -350,6 +350,7 @@ if ( is_multisite() && is_network_admin() && ! IS_PROFILE_PAGE && current_user_c
 </tr>
 
 <?php
+  if ( is_super_admin() ) {
 	foreach (_wp_get_user_contactmethods( $profileuser ) as $name => $desc) {
 ?>
 <tr>
@@ -357,6 +358,7 @@ if ( is_multisite() && is_network_admin() && ! IS_PROFILE_PAGE && current_user_c
 	<td><input type="text" name="<?php echo $name; ?>" id="<?php echo $name; ?>" value="<?php echo esc_attr($profileuser->$name) ?>" class="regular-text" /></td>
 </tr>
 <?php
+  }
 	}
 ?>
 </table>
