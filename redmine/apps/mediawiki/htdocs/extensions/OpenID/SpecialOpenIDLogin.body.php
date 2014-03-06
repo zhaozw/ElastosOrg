42d2a00795ea8815ec7ec47714e6f1fc44f79782
XilongPei <pei.xilong@kortide.com>
2014-03-06 09:23:14 +0800

    clear some runtime warning of mediawiki/OpenID

--- redmine/apps/mediawiki/htdocs/extensions/OpenID/SpecialOpenIDLogin.body.php
+++ redmine/apps/mediawiki/htdocs/extensions/OpenID/SpecialOpenIDLogin.body.php
@@ -542,8 +542,10 @@ class SpecialOpenIDLogin extends SpecialOpenID {
 				$user->setRealName( $sreg['fullname'] );
 			}
 
-			if ( array_key_exists( 'http://axschema.org/namePerson/first', $ax ) || array_key_exists( 'http://axschema.org/namePerson/last', $ax ) ) {
-				$user->setRealName( $ax['http://axschema.org/namePerson/first'][0] . " " . $ax['http://axschema.org/namePerson/last'][0] );
+			if ( ! is_null($ax) ) {
+				if ( array_key_exists( 'http://axschema.org/namePerson/first', $ax ) || array_key_exists( 'http://axschema.org/namePerson/last', $ax ) ) {
+					$user->setRealName( $ax['http://axschema.org/namePerson/first'][0] . " " . $ax['http://axschema.org/namePerson/last'][0] );
+				}
 			}
 		}
 
