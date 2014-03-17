ad8901ff349927872b2ac3ae5786e32b58e0c230
XilongPei <pei.xilong@kortide.com>
2014-03-17 10:25:49 +0800

    nl2br for bubble of Avatar

--- redmine/apps/wordpress/htdocs/wp-content/plugins/cd-bp-avatar-bubble/cd-avatar-bubble.php
+++ redmine/apps/wordpress/htdocs/wp-content/plugins/cd-bp-avatar-bubble/cd-avatar-bubble.php
@@ -346,7 +346,7 @@ function cd_ab_get_the_userdata($ID, $cd_ab) {
                     $field_link = is_array($field_value) ? implode(", ", $field_value) : $field_value;
                     $field_link = apply_filters('cd_ab_field_text', $field_link, $ID, $field_id, $field_data['type'], $field_value );
                 }
-                $output .= '<p class="popupLine"'. $class .'><strong>' . $field_data['name'] . '</strong>: ' . $field_link . '</p>';
+                $output .= '<p class="popupLine"'. $class .'><strong>' . $field_data['name'] . '</strong>: ' . nl2br($field_link) . '</p>';
             }
             $i++;
         }
