6f8a5e1a9bf2217097b63b2836c9f1f2ed120797
honggangli <li.honggang@kortide.com>
2014-03-18 13:24:30 +0800

    add BpfbSmile just like BpfbLink;support insert :) :(

--- redmine/apps/wordpress/htdocs/wp-content/plugins/buddypress-activity-plus/js/bpfb_interface.js
+++ redmine/apps/wordpress/htdocs/wp-content/plugins/buddypress-activity-plus/js/bpfb_interface.js
@@ -7,6 +7,42 @@ var $form;
 var $text;
 var $textContainer;
 
+jQuery.fn.extend({
+    getCurPos: function(){
+    	var $= jQuery;
+        var e=$(this).get(0);
+        e.focus();
+        if(e.selectionStart){    //FF
+            return e.selectionStart;
+        }
+        if(document.selection){    //IE
+            var r = document.selection.createRange();
+            if (r == null) {
+                return e.value.length;
+            }
+            var re = e.createTextRange();
+            var rc = re.duplicate();
+            re.moveToBookmark(r.getBookmark());
+            rc.setEndPoint('EndToStart', re);
+            return rc.text.length;
+        }
+        return e.value.length;
+    },
+    setCurPos: function(pos) {
+    	var $= jQuery;
+        var e=$(this).get(0);
+        e.focus();
+        if (e.setSelectionRange) {
+            e.setSelectionRange(pos, pos);
+        } else if (e.createTextRange) {
+            var range = e.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    }        
+});
 
 /**
  * Video insertion/preview handler.
@@ -239,6 +275,191 @@ var BpfbLinkHandler = function () {
 	return {"destroy": destroy, "get": processForSave};
 };
 
+/**
+ * Smile insertion/preview handler.
+ */
+var BpfbSmileHandler = function () {
+	$container = $(".bpfb_controls_container");
+
+	var resize = function () {
+		$('#bpfb_smile_preview_url').width($container.width());
+	};
+
+	var createMarkup = function () {
+		//var html = '<input type="text" id="bpfb_mile_preview_url" name="bpfb_smile_preview_url" placeholder="' + l10nBpfb.paste_smile_url + '" value="" />' +
+		//	'<input type="button" id="bpfb_smile_url_preview" value="' + l10nBpfb.preview + '" />';
+        var html=$('<ul>'+
+		'<li><img src="/wp-includes/images/smilies/icon_question.gif" alt=":?:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_razz.gif" alt=":razz:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_sad.gif" alt=":sad:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_evil.gif" alt=":evil:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_exclaim.gif" alt=":!:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_smile.gif" alt=":smile:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_redface.gif" alt=":oops:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_biggrin.gif" alt=":grin:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_surprised.gif" alt=":eek:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_eek.gif" alt=":shock:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_confused.gif" alt=":???:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_cool.gif" alt=":cool:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_lol.gif" alt=":lol:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_mad.gif" alt=":mad:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_twisted.gif" alt=":twisted:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_rolleyes.gif" alt=":roll:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_wink.gif" alt=":wink:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_idea.gif" alt=":idea:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_arrow.gif" alt=":arrow:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_neutral.gif" alt=":neutral:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_cry.gif" alt=":cry:" /></li>'+
+		'<li><img src="/wp-includes/images/smilies/icon_mrgreen.gif" alt=":mrgreen:" /></li>'+
+		'</ul>');
+		html.find("li").css({"float":"left","padding":"2px"});
+		html.find("li").click(function(){
+			value=$("#whats-new").val();
+			value=value.replace(value.substring(0,jQuery("#whats-new").getCurPos()),value.substring(0,jQuery("#whats-new").getCurPos())+$(this).find('img').attr('alt')+" ");
+			$("#whats-new").val(value);
+		});
+		$container.empty().append(html);
+
+		$(window).off("resize.bpfb").on("resize.bpfb", resize);
+		resize();
+		$('#bpfb_smile_preview_url').focus(function () {
+			$(this)
+				.select()
+				.addClass('changed')
+			;
+		});
+
+		$('#bpfb_smile_preview_url').keypress(function (e) {
+			if (13 != e.which) return true;
+			createsmilePreview();
+			return false;
+		});
+		$('#bpfb_smile_preview_url').change(createsmilePreview);
+		$('#bpfb_smile_url_preview').click(createsmilePreview);
+	};
+
+	var createPreviewMarkup = function (data) {
+		if (!data.url) {
+			$('.bpfb_preview_container').empty().html(data.title);
+			return false;
+		}
+		var imgs = '';
+		$.each(data.images, function(idx, img) {
+			if (!img) return true;
+			var url = img.match(/^http/) ? img : data.url + '/' + img;
+			imgs += '<img class="bpfb_smile_preview_image" src="' + url + '" />';
+		});
+		var html = '<table border="0">' +
+			'<tr>' +
+				'<td>' +
+					'<div class="bpfb_smile_preview_container">' +
+						imgs +
+						'<input type="hidden" name="bpfb_smile_img" value="" />' +
+					'</div>' +
+				'</td>' +
+				'<td>' +
+					'<div class="bpfb_smile_preview_title">' + data.title + '</div>' +
+					'<input type="hidden" name="bpfb_smile_title" value="' + data.title + '" />' +
+					'<div class="bpfb_smile_preview_url">' + data.url + '</div>' +
+					'<input type="hidden" name="bpfb_smile_url" value="' + data.url + '" />' +
+					'<div class="bpfb_smile_preview_body">' + data.text + '</div>' +
+					'<input type="hidden" name="bpfb_smile_body" value="' + data.text + '" />' +
+					'<div class="bpfb_thumbnail_chooser">' +
+						'<span class="bpfb_left"><img class="bpfb_thumbnail_chooser_left" src="' + _bpfb_data.root_url + '/img/system/left.gif" />&nbsp;</span>' +
+						'<span class="bpfb_thumbnail_chooser_label">' + l10nBpfb.choose_thumbnail + '</span>' +
+						'<span class="bpfb_right">&nbsp;<img class="bpfb_thumbnail_chooser_right" src="' + _bpfb_data.root_url + '/img/system/right.gif" /></span>' +
+						'<br /><input type="checkbox" id="bpfb_smile_no_thumbnail" /> <label for="bpfb_smile_no_thumbnail">' + l10nBpfb.no_thumbnail + '</label>' +
+					'</div>' +
+				'</td>' +
+			'</tr>' +
+		'</table>';
+		$('.bpfb_preview_container').empty().html(html);
+		$('.bpfb_action_container').html(
+			'<p><input type="button" class="button-primary bpfb_primary_button" id="bpfb_submit" value="' + l10nBpfb.add_smile + '" /> ' +
+			'<input type="button" class="button" id="bpfb_cancel" value="' + l10nBpfb.cancel + '" /></p>'
+		);
+		$("#bpfb_cancel_action").hide();
+
+		$('img.bpfb_smile_preview_image').hide();
+		$('img.bpfb_smile_preview_image').first().show();
+		$('input[name="bpfb_smile_img"]').val($('img.bpfb_smile_preview_image').first().attr('src'));
+
+		//$('.bpfb_thumbnail_chooser_left').click(function () {
+		$('.bpfb_thumbnail_chooser .bpfb_left').click(function () {
+			var $cur = $('img.bpfb_smile_preview_image:visible');
+			var $prev = $cur.prev('.bpfb_smile_preview_image');
+			if ($prev.length) {
+				$cur.hide();
+				$prev
+					.width($('.bpfb_smile_preview_container').width())
+					.show();
+				$('input[name="bpfb_smile_img"]').val($prev.attr('src'));
+			}
+			return false;
+		});
+		//$('.bpfb_thumbnail_chooser_right').click(function () {
+		$('.bpfb_thumbnail_chooser .bpfb_right').click(function () {
+			var $cur = $('img.bpfb_smile_preview_image:visible');
+			var $next = $cur.next('.bpfb_smile_preview_image');
+			if ($next.length) {
+				$cur.hide();
+				$next
+					.width($('.bpfb_smile_preview_container').width())
+					.show();
+				$('input[name="bpfb_smile_img"]').val($next.attr('src'));
+			}
+			return false;
+		});
+		$("#bpfb_smile_no_thumbnail").click(function () {
+			if ($("#bpfb_smile_no_thumbnail").is(":checked")) {
+				$('img.bpfb_smile_preview_image:visible').hide();
+				$('input[name="bpfb_smile_img"]').val('');
+				$(".bpfb_left, .bpfb_right, .bpfb_thumbnail_chooser_label").hide();
+			} else {
+				var $img = $('img.bpfb_smile_preview_image:first');
+				$img.show();
+				$(".bpfb_left, .bpfb_right, .bpfb_thumbnail_chooser_label").show();
+				$('input[name="bpfb_smile_img"]').val($img.attr('src'));
+			}
+
+		});
+	};
+
+	var createsmilePreview = function () {
+		var url = $('#bpfb_smile_preview_url').val();
+		if (!url) return false;
+		$('.bpfb_preview_container').html('<div class="bpfb_waiting"></div>');
+		$.post(ajaxurl, {"action":"bpfb_preview_smile", "data":url}, function (data) {
+			createPreviewMarkup(data);
+		});
+	};
+
+	var processForSave = function () {
+		return {
+			"bpfb_smile_url": $('input[name="bpfb_smile_url"]').val(),
+			"bpfb_smile_image": $('input[name="bpfb_smile_img"]').val(),
+			"bpfb_smile_title": $('input[name="bpfb_smile_title"]').val(),
+			"bpfb_smile_body": $('input[name="bpfb_smile_body"]').val()
+		};
+	};
+
+	var init = function () {
+		$('#aw-whats-new-submit').hide();
+		createMarkup();
+	};
+
+	var destroy = function () {
+		$container.empty();
+		$('.bpfb_preview_container').empty();
+		$('.bpfb_action_container').empty();
+		$('#aw-whats-new-submit').show();
+		$(window).off("resize.bpfb");
+	};
+
+	init ();
+
+	return {"destroy": destroy, "get": processForSave};
+};
 
 /**
  * Photos insertion/preview handler.
@@ -385,6 +606,8 @@ function createMarkup () {
 			'<a href="#videos" class="bpfb_toolbarItem" title="' + l10nBpfb.add_videos + '" id="bpfb_addVideos"><span>' + l10nBpfb.add_videos + '</span></a>' +
 			'&nbsp;' +
 			'<a href="#links" class="bpfb_toolbarItem" title="' + l10nBpfb.add_links + '" id="bpfb_addLinks"><span>' + l10nBpfb.add_links + '</span></a>' +
+			'&nbsp;' +
+			'<a href="#smiles" class="" title="' + 'smiles' + '" id="bpfb_addSmiles"><span>' + 'smiles' + '</span></a>' +
 		'</div>' +
 		'<div class="bpfb_controls_container">' +
 		'</div>' +
@@ -417,6 +640,13 @@ function init () {
 		if (_bpfbActiveHandler) _bpfbActiveHandler.destroy();
 		_bpfbActiveHandler = new BpfbLinkHandler();
 		$("#bpfb_cancel_action").show();
+		return false;
+	});
+	$('#bpfb_addSmiles').click(function () {
+		if (_bpfbActiveHandler) _bpfbActiveHandler.destroy();
+		_bpfbActiveHandler = new BpfbSmileHandler();
+		//$("#bpfb_cancel_action").show();
+		$("#aw-whats-new-submit").show();
 		return false;
 	});
 	$('#bpfb_addVideos').click(function () {
