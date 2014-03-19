c5a875340aae4b76c0612bcb17f1748afdbbd1f5
XilongPei <pei.xilong@kortide.com>
2014-03-19 15:28:46 +0800

    BuddyPress plugin, Activity Shortcode

--- redmine/apps/wordpress/htdocs/wp-content/plugins/bp-activity-shortcode/bp-activity-as-shortcode.php
+++ redmine/apps/wordpress/htdocs/wp-content/plugins/bp-activity-shortcode/bp-activity-as-shortcode.php
@@ -4,7 +4,7 @@
  * Author: Brajesh Singh(BuddyDev)
  * Plugin URI: http://buddydev.com/plugins/bp-activity-shortcode/
  * Author URI: http://buddydev.com/members/sbrajesh/
- * Version: 1.0.2
+ * Version: 99.1.0.2
  * License: GPL
  */
 class BD_Activity_Stream_Shortcodes_Helper{
@@ -43,21 +43,19 @@ class BD_Activity_Stream_Shortcodes_Helper{
 
         return self::$instance;
     }
-    
-    
 
     public function generate_activity_stream( $atts, $content = null ) {
         //allow to use all those args awesome!
        $atts=shortcode_atts(array(
-            'title'            => 'Latest Activity',//title of the section
-            'pagination'       => 'true',//show or not
+            'title'            => false,
+            'pagination'       => 'true',	 //show or not
             'display_comments' => 'threaded',
             'include'          => false,     // pass an activity_id or string of IDs comma-separated
             'exclude'          => false,     // pass an activity_id or string of IDs comma-separated
             'in'               => false,     // comma-separated list or array of activity IDs among which to search
             'sort'             => 'DESC',    // sort DESC or ASC
             'page'             => 1,         // which page to load
-            'per_page'         => 5,         //how many per page
+            'per_page'         => 10,        // how many per page
             'max'              => false,     // max number to return
 
             // Scope - pre-built activity filters for a user (friends/groups/favorites/mentions)
@@ -72,36 +70,71 @@ class BD_Activity_Stream_Shortcodes_Helper{
 
             // Searching
             'search_terms'     => false,         // specify terms to search on
-            'use_compat'       => bp_use_theme_compat_with_current_theme() 
         ), $atts );
        
         extract( $atts );
-	
-      
-        
+
         ob_start(); ?>
-	
-    <?php if( $use_compat):?>
+
+    <?php /* if( $use_compat): */ ?>
         <div id="buddypress">
-    <?php endif;?>		
+    <?php /* endif; */ ?>
+		
 	<?php if($title): ?>
             <h3 class="activity-shortcode-title"><?php echo $title; ?></h3>
         <?php endif;?>    
+
+<div>&nbsp;</div>
+<div class="item-list-tabs no-ajax" id="subnav" role="navigation">
+	<ul>
+		<?php $search_value = !empty( $_REQUEST['search_terms'] ) ? stripslashes( $_REQUEST['search_terms'] ) : 'Filter contents'; ?><li class="feed"  style="list-style:none;">
+		<form action="" method="get" id="search-contents-form">
+		<input type="submit" id="contents_search_submit" name="contents_search_submit" value="<?php _e( 'Filter', 'buddypress' ) ?>" />
+		<label><input type="text" name="search_terms" id="content_search" placeholder="<?php echo esc_attr( $search_value ) ?>" /></label>
+		</form>
+		</li>
+		<?php do_action( 'bp_group_activity_syndication_options' ); ?>
+
+		<li id="activity-filter-select" class="last"  style="list-style:none;">
+			<label for="activity-filter-by"><?php _e( 'Show:', 'buddypress' ); ?></label> 
+			<select id="activity-filter-by">
+				<option value="-1"><?php _e( 'Everything', 'buddypress' ); ?></option>
+				<option value="activity_update"><?php _e( 'Updates', 'buddypress' ); ?></option>
+
+				<?php if ( bp_is_active( 'forums' ) ) : ?>
+					<option value="new_forum_topic"><?php _e( 'Forum Topics', 'buddypress' ); ?></option>
+					<option value="new_forum_post"><?php _e( 'Forum Replies', 'buddypress' ); ?></option>
+				<?php endif; ?>
+
+				<option value="joined_group"><?php _e( 'Group Memberships', 'buddypress' ); ?></option>
+
+				<?php do_action( 'bp_group_activity_filter_options' ); ?>
+			</select>
+		</li>
+	</ul>
+</div><!-- .item-list-tabs -->
 		
         <?php do_action( 'bp_before_activity_loop' ); ?>
 
         <?php if ( bp_has_activities($atts)  ) : ?>
             <div class="activity <?php if(!$display_comments): ?> hide-activity-comments<?php endif; ?> shortcode-activity-stream">
 
+                <?php if($pagination):?>
+                    <div class="pagination">
+                        <div class="pag-count"><?php bp_activity_pagination_count(); ?></div>
+                        <div class="pagination-links"><?php bp_activity_pagination_links(); ?></div>
+                    </div>
+                <?php endif;?>
+
                  <?php if ( empty( $_POST['page'] ) ) : ?>
 
-                    <ul id="activity-stream" class="activity-list item-list">
+                    <ul id="activity-stream" class="activity-list item-list" style="list-style:none;">
 
                  <?php endif; ?>
 
                  <?php while ( bp_activities() ) : bp_the_activity(); ?>
 
-                    <?php bp_get_template_part( 'activity/entry'); ?>
+                    <?php locate_template( array( 'activity/entry.php' ), true, false ); ?>
 
                  <?php endwhile; ?>
 
@@ -134,9 +167,9 @@ class BD_Activity_Stream_Shortcodes_Helper{
         <?php wp_nonce_field( 'activity_filter', '_wpnonce_activity_filter' ); ?>
 
     </form>
-     <?php if( $use_compat ):?>       
+     <?php /* if( $use_compat ): */ ?>       
         </div>
-     <?php endif;?>
+     <?php /* endif; */ ?>
     <?php 
 
 	$output = ob_get_clean();
