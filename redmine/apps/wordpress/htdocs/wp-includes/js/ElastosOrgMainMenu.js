27f80a9e12b8ef340c614a79d81514f2b14f96ed
honggangli <li.honggang@kortide.com>
2014-04-01 09:57:34 +0800

    jQuery(\'#bpaa-form-wrapper\').drag

--- redmine/apps/wordpress/htdocs/wp-includes/js/ElastosOrgMainMenu.js
+++ redmine/apps/wordpress/htdocs/wp-includes/js/ElastosOrgMainMenu.js
@@ -76,3 +76,16 @@ function showMessage(msg){
     msgBox.append(msg);
     setTimeout(function(){msgBox.fadeOut(500,function(){msgBox.remove();})},3000)
 }
+
+/*!
+   * jquery.fn.drag
+   * @author ..... http://qianduanblog.com
+   * @for ie9(..)+..hrome..irefox
+   * @version 1.1
+   * 2013-11-23 16:24:47
+   */
+  (function(a,c){var b={handle:"",ondragstart:function(){},ondrag:function(){},ondragend:f    unction(){}};a.fn.drag=function(d){return this.each(function(){var n,i=a(this),p=a.exten    d({},b,d),f=p.handle?i.find(p.handle):i,m=a(p.zoom),k={},o=a("body").css("cursor"),l=0,j    =0;f.css("cursor","move");g(i);f.mousedown(function(q){if(!j){l=i.css("z-index");h(q);re    turn false;}});a(document).mousemove(function(q){if(j){e(q);return false;}}).mouseup(fun    ction(q){if(j){e(q,true);return false;}});function g(){if(!i.css("position")||i.css("pos    ition")=="static"){i.css({position:"relative",left:0,top:0});}}function h(q){j=1;k.scree    nX=q.screenX;k.screenY=q.screenY;k.left=i.offset().left;k.top=i.offset().top;i.css("z-in    dex",a.isNumeric(l)?l*1+1:1);p.ondragstart.call(i,q);a("body").css("cursor","move");}fun    ction e(r,q){if(j){i.offset({left:r.screenX-k.screenX+k.left,top:r.screenY-k.screenY+k.t    op});}if(q&&j){i.css("z-index",l);j=0;p.ondragend.call(i,r);a("body").css("cursor",o);}e    lse{p.ondrag.call(i,r);}}});};a.fn.drag.defaults=b;})(jQuery);
+ 
+jQuery(function(){
+    jQuery("#bpaa-form-wrapper").drag({ondragend:function(){jQuery("#bpaa-textarea").focus()    ;}})
+});
