53a8d75a7acd6e25673ecccf80170ec55c1cc0d0
XilongPei <pei.xilong@kortide.com>
2014-04-23 10:44:23 +0800

    custom sidebar min-height

--- redmine/apps/wordpress/htdocs/wp-content/plugins/buddypress/bp-themes/bp-default/sidebar.php
+++ redmine/apps/wordpress/htdocs/wp-content/plugins/buddypress/bp-themes/bp-default/sidebar.php
@@ -20,6 +20,6 @@
 	</div><!-- .padder -->
 </div><!-- #sidebar -->
 <script>
-jQuery("#sidebar").css({height:jQuery("#content").height(),"min-height":jQuery("#sidebar>.padder").height()+50,"background-color":"#ECF4F6"});
+jQuery("#sidebar").css({height:jQuery("#content").height(),"min-height":jQuery("#sidebar>.padder").height()+200,"background-color":"#ECF4F6"});
 </script>
 <?php do_action( 'bp_after_sidebar' ); ?>
