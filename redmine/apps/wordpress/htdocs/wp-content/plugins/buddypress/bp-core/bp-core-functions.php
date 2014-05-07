b0826eb4d293ffe2e9c2da7e505c549d988b045c
XilongPei <pei.xilong@kortide.com>
2014-05-07 11:27:14 +0800

    Nginx, proxy_pass http: //clusterR$request_uri;

--- redmine/apps/wordpress/htdocs/wp-content/plugins/buddypress/bp-core/bp-core-functions.php
+++ redmine/apps/wordpress/htdocs/wp-content/plugins/buddypress/bp-core/bp-core-functions.php
@@ -388,13 +388,13 @@ function bp_core_time_since( $older_date, $newer_date = false ) {
 
 	// array of time period chunks
 	$chunks = array(
-		array( 60 * 60 * 24 * 365 , __( 'Y', 'buddypress' ), __( 'Y', 'buddypress' ) ),
-		array( 60 * 60 * 24 * 30 ,  __( 'M', 'buddypress' ), __( 'M', 'buddypress' ) ),
-		array( 60 * 60 * 24 * 7,    __( 'w', 'buddypress' ), __( 'w', 'buddypress' ) ),
-		array( 60 * 60 * 24 ,       __( 'd', 'buddypress' ), __( 'd', 'buddypress' ) ),
-		array( 60 * 60 ,            __( 'h', 'buddypress' ), __( 'h', 'buddypress' ) ),
-		array( 60 ,                 __( 'm', 'buddypress' ), __( 'm', 'buddypress' ) ),
-		array( 1,                   __( 's', 'buddypress' ), __( 's', 'buddypress' ) )
+		array( 60 * 60 * 24 * 365 , __( 'y', 'buddypress' ),   __( 'y', 'buddypress' ) ),
+		array( 60 * 60 * 24 * 30 ,  __( 'mo', 'buddypress' ),  __( 'mo', 'buddypress' ) ),
+		array( 60 * 60 * 24 * 7,    __( 'wk', 'buddypress' ),  __( 'wk', 'buddypress' ) ),
+		array( 60 * 60 * 24 ,       __( 'd', 'buddypress' ),   __( 'd', 'buddypress' ) ),
+		array( 60 * 60 ,            __( 'h', 'buddypress' ),   __( 'h', 'buddypress' ) ),
+		array( 60 ,                 __( 'min', 'buddypress' ), __( 'min', 'buddypress' ) ),
+		array( 1,                   __( 's', 'buddypress' ),   __( 's', 'buddypress' ) )
 	);
 
 	if ( !empty( $older_date ) && !is_numeric( $older_date ) ) {
