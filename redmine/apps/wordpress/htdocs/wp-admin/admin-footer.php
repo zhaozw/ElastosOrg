4cd13fd790944adb1102d43cf149d38b4c386595
XilongPei <pei.xilong@kortide.com>
2014-05-20 15:19:56 +0800

    fix a bug

--- redmine/apps/wordpress/htdocs/wp-admin/admin-footer.php
+++ redmine/apps/wordpress/htdocs/wp-admin/admin-footer.php
@@ -42,8 +42,13 @@ if ( function_exists('get_site_option') ) {
 	if ( false === get_site_option('can_compress_scripts') )
 		compression_test();
 }
+*/
 
-*/ ?>
+do_action('admin_footer', '');
+do_action('admin_print_footer_scripts');
+do_action("admin_footer-" . $GLOBALS['hook_suffix']);
+
+?>
 
 <div class="clear"></div></div><!-- wpwrap -->
 <script type="text/javascript">if(typeof wpOnload=='function')wpOnload();</script>
